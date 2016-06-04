81c1d9ec1e733d79c2faf65d377265ea1c06baf3
Thorsten Schäfer <thorstenschaefer@outlook.com>
2016-06-04 13:31:03 -0300

    ng2 maps

--- src/app/proximity.component.ts
+++ src/app/proximity.component.ts
@@ -4,18 +4,22 @@ import { PlacesService } from './places.service';
 import { Place } from './place';
 
 import { PlacesListComponent } from './places-list/places-list.component';
+import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core';
 
 @Component({
   moduleId: module.id,
   selector: 'proximity-app',
   templateUrl: 'proximity.component.html',
   providers: [PlacesService],
-  directives: [PlacesListComponent]
+  directives: [PlacesListComponent, ANGULAR2_GOOGLE_MAPS_DIRECTIVES],
+  styleUrls: ['proximity.component.css']
 })
 export class ProximityAppComponent {
 
   places:Place[] = [];
-  
+    lat: number = 51.678418;
+  lng: number = 7.809007;
+
   constructor(placesService:PlacesService) {
     this.places = placesService.getPlaces("");
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { ProximityAppComponent, environment } from './app/';
+import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(ProximityAppComponent);
+bootstrap(ProximityAppComponent, [ANGULAR2_GOOGLE_MAPS_PROVIDERS]);
